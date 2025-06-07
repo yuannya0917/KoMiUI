2b7ed08cc2b4ed01f216a0b3712d63e62ce81118
yuan lu <2766508815@qq.com>
2025-06-07 21:48:16 +0800

    迁移到本地C盘 进行npm link测试

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { library} from "@fortawesome/fontawesome-svg-core";
+import { faS } from "@fortawesome/free-solid-svg-icons";
+import Icon from "./components/Icon";
+library.add(faS)
+
+
+
+export {default as Button} from "./components/Button";
+export {default as Menu} from "./components/Menu";
+export {default as Alert} from "./components/Alert";
+export {default as AutoComplete} from "./components/AutoComplete";
+export {default as Select} from "./components/Select";
+export {default as Tabs} from "./components/Tabs";
+export {default as Upload} from "./components/Upload";
+export {default as Icon} from "./components/Icon"
+export {default as Progress} from "./components/Progress"
+export {default as Transition} from "./components/Transition"
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import {default as Button} from "./components/Button";
-import {default as Menu} from "./components/Menu";
-import {default as Alert} from "./components/Alert";
-import {default as AutoComplete} from "./components/AutoComplete";
-import {default as Select} from "./components/Select";
-import {default as Tabs} from "./components/Tabs";
-import {default as Upload} from "./components/Upload";
